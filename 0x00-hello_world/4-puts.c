41a203c012fb58bce978a76e141404f22bd36c1c
Iwairimu <wairimuchuchu@gmail.com>
2023-02-16 16:17:48 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - A program that prints a line with puts function
+ *Return: O(success)
+ */
+int main (void)
+{
+puts ("\"Programmig is like building a multiligual puzzel");
+return (O)
+}
