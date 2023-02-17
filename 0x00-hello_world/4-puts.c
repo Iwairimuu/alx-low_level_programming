182586a3e7283f04b72f0fbf0700eeecebf318ac
Iwairimu <wairimuchuchu@gmail.com>
2023-02-16 16:34:53 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *main - A program that prints a line with puts function
  *Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 puts("\"Programmig is like building a multiligual puzzel");
 return (0);
