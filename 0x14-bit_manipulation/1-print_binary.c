fa56bff6e631585094549b26522cdf22f6370819
Iwairimu <wairimuchuchu@gmail.com>
2023-04-10 06:07:29 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,11 +6,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	for (i = 63; i >= 0; i--)
+	int i=63, count = 0;
+	unsigned long int current;
+
+	while (i >= 0)
 	{
-	       if ((n >> i) & 1)
-		       _putchar ('1');
-		else
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
 			_putchar('0');
+
+		 i--;
+	}
+	if (!count)
+		_putchar('0');
 }
